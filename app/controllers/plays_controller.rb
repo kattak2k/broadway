bcb1d4bd212adc3b9d371fb964e74d215d1909d3
kattak2k <kkiran.ror@gmail.com>
2017-04-19 04:43:27 +0530

    add plays show and index views

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,19 +1,34 @@
 class PlaysController < ApplicationController
-    def index
-     
-    end
+    before_action :find_play, only: [:show,:edit,:update,:destroy] 
     
+    def index
+     @plays = Play.order(created_at: :desc)
+    end 
+    
+    def show
+      
+    end
     def new
         @play = Play.new
     end
 
     def create
         @play = Play.new(play_params)
+
+        if @play.save
+            redirect_to root_path
+        else
+            render 'new'
+        end 
     end
 
 private
 
     def play_params
      params.require(:play).permit(:title, :description, :director)
+    end    
+
+    def find_play
+     @play = Play.find(params[:id])  
     end    
 end
