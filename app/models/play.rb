b964463b9f168dcc4a730556fe1dbd89b2be6012
kattak2k <kkiran.ror@gmail.com>
2017-04-23 00:56:25 +0530

    add images to the index page

--- app/models/play.rb
+++ app/models/play.rb
@@ -2,6 +2,6 @@ class Play < ApplicationRecord
     belongs_to :user
     belongs_to :category
 
-  has_attached_file :play_img, styles: { play_index: "250x350>",  play_index: "325x475>" }
+  has_attached_file :play_img, styles: { play_index: "250x350>",  play_show: "325x475>" }
   validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
 end
