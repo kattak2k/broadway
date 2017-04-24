0c5d93fbd119327e9ceab08de45ee4674f53d273
kattak2k <kkiran.ror@gmail.com>
2017-04-25 05:11:05 +0530

    add average rating

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,6 +1,6 @@
 class PlaysController < ApplicationController
     before_action :find_play, only: [:show,:edit,:update,:destroy] 
-    
+    before_action :authenticate_user! , only: [:new, :edit]
     def index
         if params[:category].blank? 
             @plays = Play.order(created_at: :desc)
@@ -12,6 +12,11 @@ class PlaysController < ApplicationController
     end 
 
     def show
+        if @play.reviews.blank?
+            @average_review = 0
+        else
+            @average_review = @play.review.average(:rating).round(2)
+        end    
     end
 
     def new
