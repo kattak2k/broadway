69852234223b1abc5a0494486f2791dc24965d61
kattak2k <kkiran.ror@gmail.com>
2017-04-25 05:20:49 +0530

    add average ratings on show

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -15,7 +15,7 @@ class PlaysController < ApplicationController
         if @play.reviews.blank?
             @average_review = 0
         else
-            @average_review = @play.review.average(:rating).round(2)
+            @average_review = @play.reviews.average(:rating).round(2)
         end    
     end
 
